dd5092f50e00a6e159a293fcc0cdf688837591fb
Pascale Pierre <pascaleee@gmail.com>
2018-04-11 14:16:06 -0500

    completed lesson 5 homework

--- Lesson5-JS-II/homework/homework.js
+++ Lesson5-JS-II/homework/homework.js
@@ -1,17 +1,43 @@
 // Do not change any of the function names
 
 function multiplyArguments() {
-  // use the arguments keyword to multiply all of the arguments together and return the product
-  // if no arguments are passed in return 0
-  // if one argument is passed in just return it
+
+  var product = 1;
+
+  if (arguments.length === 0) {
+    return 0;
+  } else if (arguments.length === 1) {
+    return arguments[0];
+  }
+
+  for (var i = 0; i < arguments.length; i++) {
+    product *= arguments[i];
+  }
+  return product;
 }
 
+// use the arguments keyword to multiply all of the arguments together and return the product
+// if no arguments are passed in return 0
+// if one argument is passed in just return it
+
 function getBiggest(x, y) {
+  var greaterList = [x, y];
+  var sortedList = greaterList.sort();
+  return sortedList[0];
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 }
 
 function greeting(language) {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'English') {
+    return 'Hello!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else {
+    return 'Hello!';
+  }
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'English' -> 'Hello!'
@@ -20,6 +46,11 @@ function greeting(language) {
 }
 
 function isTenOrFive(num) {
+  if (num === 10 || num === 5) {
+    return true;
+  } else {
+    return false;
+  }
   // return true if num is 10 or 5
   // otherwise return false
 }
@@ -27,9 +58,19 @@ function isTenOrFive(num) {
 function isInRange(num) {
   // return true if num is less than 50 and greater than 20
   // otherwise return false
+  if (num < 50 && num > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isInteger(num) {
+  if (Number.isInteger(num)) {
+    return true;
+  } else {
+    return false;
+  }
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
@@ -39,6 +80,15 @@ function isInteger(num) {
 }
 
 function fizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else {
+    return num;
+  }
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
@@ -46,14 +96,18 @@ function fizzBuzz(num) {
 }
 
 function isPrime(num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
   // return true if num is prime.
   // otherwise return false
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
 }
-
-
 
 // Do not modify code below this line.
 // --------------------------------
@@ -66,5 +120,5 @@ module.exports = {
   isInRange: isInRange,
   isInteger: isInteger,
   fizzBuzz: fizzBuzz,
-  isPrime: isPrime,
+  isPrime: isPrime
 };
